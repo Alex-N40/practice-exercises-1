fd09f147216a2428553d177817595c0ac1a73305
Alex-N40 <alex_n40@proton.me>
2024-02-28 13:52:09 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Write your JavaScript below this line
+//Exercise 1
+var numberOfHorses = 15;
+console.log(numberOfHorses);
+
+//Exercise 2
+var favoriteFlower = "peony";
+console.log("My favorite flower is a " + favoriteFlower + ".");
+
+//Exercise 3
+var birthCity = "Camas";
+var birthday = "July 5, 1982";
+console.log("I was born in " + birthCity + " on " + birthday + ".");
+
+/* Results in console:
+15
+My favorite flower is a peony.
+I was born in Camas on July 5, 1982. */
